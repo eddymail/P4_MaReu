095703dba6f21eb6aff8e6a84314d185eaf9c60a
Edouard Loussouarn <ed.loussouarn@gmail.com>
2020-04-17 22:27:00 +0200

    menu_activity_list_meeting.xml
    icone filtre

--- app/src/main/java/com/lousssouarn/edouard/mareu/ListMeetingActivity.java
+++ app/src/main/java/com/lousssouarn/edouard/mareu/ListMeetingActivity.java
@@ -1,8 +1,11 @@
 package com.lousssouarn.edouard.mareu;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class ListMeetingActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class ListMeetingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_meeting);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionBar=getSupportActionBar();
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_activity_list_meeting,menu);
+        return true;
     }
 }
