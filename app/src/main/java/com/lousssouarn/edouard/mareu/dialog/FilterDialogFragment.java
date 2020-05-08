6213e1e1baf637f8251660caa212e238d8f7fbc0
Edouard Loussouarn <ed.loussouarn@gmail.com>
2020-05-08 10:18:08 +0200

    FilterDialog   Spinner Ok   DateTimePicker: toDo display start / end

--- app/src/main/java/com/lousssouarn/edouard/mareu/dialog/FilterDialogFragment.java
+++ app/src/main/java/com/lousssouarn/edouard/mareu/dialog/FilterDialogFragment.java
@@ -1,17 +1,19 @@
 package com.lousssouarn.edouard.mareu.dialog;
 
-import android.app.AlertDialog;
-import android.content.Context;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
+
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,11 +21,14 @@ import androidx.fragment.app.DialogFragment;
 
 import com.lousssouarn.edouard.mareu.R;
 
-import java.util.Objects;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class FilterDialogFragment extends DialogFragment {
     private String room;
     Spinner mRoomInput;
+    EditText mStartDate;
+    EditText mEndDate;
 
 
     @Nullable
@@ -34,7 +39,7 @@ public class FilterDialogFragment extends DialogFragment {
         //Spinner meeting room list
         mRoomInput = view.findViewById(R.id.sp_room);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.nameRooms, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.filterRooms, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mRoomInput.setAdapter(adapter);
 
@@ -42,15 +47,63 @@ public class FilterDialogFragment extends DialogFragment {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 room = parent.getItemAtPosition(position).toString();
-                
-            }
 
+            }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //DateTimePikerDialog
+        mEndDate = view.findViewById(R.id.et_end);
+        mStartDate = view.findViewById(R.id.et_start);
+        mStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateTimeDialog(mStartDate);
+                mStartDate.isSelected();
+            }
+        });
+        mEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateTimeDialog(mEndDate);
             }
         });
 
         return view;
     }
 
+    private void showDateTimeDialog(EditText start) {
+        final Calendar calendar = Calendar.getInstance();
+
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                //Set the Date Value into Calendar
+                calendar.set(Calendar.YEAR,year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                        calendar.set(Calendar.MINUTE,minute);
+
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy à HH:mm");
+
+                        if (mStartDate.isSelected() ) {
+                            mStartDate.setText(simpleDateFormat.format(calendar.getTime()));
+                        }else {
+                            mEndDate.setText(simpleDateFormat.format(calendar.getTime()));
+                        }
+                    }
+                };
+                new TimePickerDialog(getView().getContext(), timeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show();
+            }
+        };
+        new DatePickerDialog(getView().getContext(), dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+
+    }
 }
