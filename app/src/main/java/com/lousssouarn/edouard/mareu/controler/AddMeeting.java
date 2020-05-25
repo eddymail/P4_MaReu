a42623b52d20c16c6bbcb61618e57db3edab70df
Edouard Loussouarn <ed.loussouarn@gmail.com>
2020-05-25 11:15:38 +0200

    finish

--- app/src/main/java/com/lousssouarn/edouard/mareu/controler/AddMeeting.java
+++ app/src/main/java/com/lousssouarn/edouard/mareu/controler/AddMeeting.java
@@ -28,7 +28,12 @@ public class AddMeeting extends AppCompatActivity implements AdapterView.OnItemS
     private MeetingApiService mApiService;
     private int newMeetingColor;
     private String room;
+    private int color;
     private String date;
+    private String time;
+    private String name;
+    private String participants;
+
 
     Spinner mSpinner;
     EditText mNameInput;
@@ -58,15 +63,6 @@ public class AddMeeting extends AppCompatActivity implements AdapterView.OnItemS
         mSpinner.setAdapter(adapter);
         mSpinner.setOnItemSelectedListener(this);
 
-        //add new meeting when button is clicked
-        mButtonNewMeeting = findViewById(R.id.bt_new_meeting);
-        mButtonNewMeeting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createMeeting();
-            }
-        });
-
         //edit Date and Time
         mDateInput.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,28 +77,28 @@ public class AddMeeting extends AppCompatActivity implements AdapterView.OnItemS
             }
         });
 
-    }
-
-    private void ShowTimeDialog(EditText editTextTimeInput) {
-        final Calendar calendar = Calendar.getInstance();
-        //creating a TimeSet Listener
-        TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        //add new meeting when button is clicked
+        mButtonNewMeeting = findViewById(R.id.bt_new_meeting);
+        mButtonNewMeeting.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            //Set the Time Value into Calendar
-                calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
-                calendar.set(Calendar.MINUTE,minute);
-                //Formatting the Time
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH'h'mm");
-                //Setting Time into EditText
-                mTimeInput.setText(simpleDateFormat.format(calendar.getTime()));
+            public void onClick(View v) {
+                createMeeting();
             }
-        };
-        //creating TimePiker Dialog
-        new  TimePickerDialog(AddMeeting.this, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show();
+        });
     }
 
-    private void showDateDialog(EditText editTextDateInput) {
+    //Get the meeting room chose
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        room = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+        private void showDateDialog(EditText editTextDateInput) {
         final Calendar calendar = Calendar.getInstance();
         //creating a DateSet Listener
         DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
@@ -121,6 +117,25 @@ public class AddMeeting extends AppCompatActivity implements AdapterView.OnItemS
 
         //creating DatePicker Dialog
         new DatePickerDialog(AddMeeting.this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    private void ShowTimeDialog(EditText editTextTimeInput) {
+        final Calendar calendar = Calendar.getInstance();
+        //creating a TimeSet Listener
+        TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                //Set the Time Value into Calendar
+                calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                calendar.set(Calendar.MINUTE,minute);
+                //Formatting the Time
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH'h'mm");
+                //Setting Time into EditText
+                mTimeInput.setText(simpleDateFormat.format(calendar.getTime()));
+            }
+        };
+        //creating TimePiker Dialog
+        new  TimePickerDialog(AddMeeting.this, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show();
     }
 
     //Get the color of the room according to the chosen room
@@ -168,29 +183,16 @@ public class AddMeeting extends AppCompatActivity implements AdapterView.OnItemS
         mTimeInput = findViewById(R.id.et_time_input);
         mParticipantsInput = findViewById(R.id.et_participants);
 
-        date =  mDateInput.getText().toString();
-        int color = getRoomColor(mSpinner);
+        color = getRoomColor(mSpinner);
+        name = mNameInput.getText().toString();
+        date = mDateInput.getText().toString();
+        time = mTimeInput.getText().toString();
+        participants = mParticipantsInput.getText().toString();
 
-        Meeting meeting = new Meeting(
-                color,
-                mNameInput.getText().toString(),
-                date,
-                mTimeInput.getText().toString(),
-                room,
-                mParticipantsInput.getText().toString()
-        );
+        Meeting meeting = new Meeting(color,name, date, time, room, participants);
+
         mApiService.addMeeting(meeting);
         finish();
     }
 
-    //Get the meeting room chose
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        room = parent.getItemAtPosition(position).toString();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 }
