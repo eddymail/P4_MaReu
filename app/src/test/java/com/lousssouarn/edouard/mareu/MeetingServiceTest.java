c1c403e35911747ab5f558360267db979310c4a9
Edouard Loussouarn <ed.loussouarn@gmail.com>
2020-07-21 11:59:31 +0200

    final

--- app/src/test/java/com/lousssouarn/edouard/mareu/MeetingServiceTest.java
+++ app/src/test/java/com/lousssouarn/edouard/mareu/MeetingServiceTest.java
@@ -5,7 +5,6 @@ import com.lousssouarn.edouard.mareu.model.Meeting;
 import com.lousssouarn.edouard.mareu.service.DummyMeetingGenerator;
 import com.lousssouarn.edouard.mareu.service.MeetingApiService;
 
-
 import org.hamcrest.collection.IsIterableContainingInAnyOrder;
 import org.junit.Assert;
 import org.junit.Before;
@@ -54,20 +53,20 @@ public class MeetingServiceTest {
 
     @Test
     public void addMeetingNotFullCompleted() {
-        Meeting meetingToAdd = new Meeting(0xFFEF5350,"", "", "","Choisir une salle","");
+        Meeting meetingToAdd = new Meeting(0xFFEF5350,"Test meeting", "10-05-2020", "","Choisir une salle","");
         try{
         mService.addMeeting(meetingToAdd);
             Assert.fail("Exception should occur");
-        }catch (IllegalArgumentException e) {
-
-        }
+        }catch (IllegalArgumentException e) {}
     }
 
     @Test
     public void filterMeetingByRoomWithSuccess() {
-        Meeting meetingTakesPlaceInBangkok = new Meeting(0xFFEF5350,"Test meeting", "10-05-2020", "09h00","Bangkok","eddy@lamzon.com");
+        Meeting meetingTakesPlaceInBangkok = new Meeting(0xFFEF5350,"Test meeting", "10-05-2020", "09h00",
+                "Bangkok","eddy@lamzon.com");
         mService.addMeeting(meetingTakesPlaceInBangkok);
         assertFalse(mService.getMeetingsByRoomName("Paris").contains(meetingTakesPlaceInBangkok));
+        assertTrue(mService.getMeetingsByRoomName("Bangkok").contains(meetingTakesPlaceInBangkok));
     }
 
     @Test
@@ -75,5 +74,6 @@ public class MeetingServiceTest {
         Meeting meetingTakesPlaceOnMayEleven = new Meeting(0xFFAB47BC,"Réunion Projet X","11-05-2020","10h00","Paris","leila@lamzone.com,mathieu@lamzone.com");
         mService.addMeeting(meetingTakesPlaceOnMayEleven);
         assertFalse(mService.getMeetingsByDate("10-05-2020").contains(meetingTakesPlaceOnMayEleven));
+        assertTrue(mService.getMeetingsByDate("11-05-2020").contains(meetingTakesPlaceOnMayEleven));
     }
 }
